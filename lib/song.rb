1a04374c044b47051718a0a3f426840e2062bc84
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-14 17:58:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,42 +5,30 @@ class Song
   @@count = 0
   @@genres = []
   @@artists = []
-  @@genre_count = Hash.new
-  @@artists_count = Hash.new
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    add_genre(genre)
-    add_artist(artist)
+    @@genres << genre
+    @@artists << artist
     @@count += 1
   end
 
-  def add_genre(genre)
-    @@genre_count.has_key?(genre) ? @@genre_count[genre] += 1 : @@genre_count[genre] = 1
-    @@genres << genre
-  end
-
-  def add_artist(artist)
-    @@artists_count.has_key?(artist) ? @@artists_count[artist] += 1 : @@artists_count[artist] = 1
-    @@artists << artist
-  end
 
   def self.count
     @@count
   end
 
   def self.genres
-    @@genres.to_set
+    @@genres << genre
   end
 
   def self.genre_count
-    @@genres.size
+    @@genres.to_set.size
   end
 
   def self.genre_count
-    @@genre_count
   end
 
   def self.artists
@@ -48,6 +36,5 @@ class Song
   end
 
   def self.artist_count
-    @@artists_count
   end
 end
