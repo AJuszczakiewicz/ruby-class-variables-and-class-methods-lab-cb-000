add2f72c5b25c1e8c4f5694721e36a15a52148a1
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-14 17:18:18 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :name, :genre, :artist
+  @@song_count = 0
+  @@genres = Set.new
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+    @@song_count += 1
+  end
+
+  def self.count
+    @@song_count
+  end
+
+  def self.genres
+    @@genres
+  end
+
+end
